2a67e21a9be08e33ee3b8fc7fa83ba7bc339e811
AmitMaish <83043520+AmitMaish@users.noreply.github.com>
2025-05-02 13:16:35 -0700

    updated teacup

--- teacup/src/layout.rs
+++ teacup/src/layout.rs
@@ -16,6 +16,12 @@ impl Container for UI {
             container.fit_sizing()
         }
     }
+
+    fn set_child_positions(&mut self) {
+        if let Ok(mut root) = self.root_item.lock() {
+            root.set_child_positions();
+        }
+    }
 }
 
 #[derive(Default)]
@@ -48,10 +54,18 @@ impl Container for TCContainer {
             "TCContainer can't compute layout as it is just a temp struct. replace with a proper container"
         )
     }
+
+    fn set_child_positions(&mut self) {
+        log!(
+            Level::Error,
+            "TCContainer can't compute layout as it is just a temp struct. replace with a proper container"
+        )
+    }
 }
 
 pub trait Container {
     fn fit_sizing(&mut self);
+    fn set_child_positions(&mut self);
 }
 
 pub trait Primative {
@@ -78,6 +92,9 @@ pub trait Primative {
     fn set_max_height(&mut self, height: Option<u16>);
     fn set_perfered_height(&mut self, height: u16);
 
+    fn get_position(&self) -> (u16, u16);
+    fn set_position(&mut self, position: (u16, u16));
+
     fn as_container(&mut self) -> Option<&mut dyn Container> {
         None
     }
@@ -92,10 +109,12 @@ pub struct Rectangle {
     min_height: u16,
     max_width: Option<u16>,
     max_height: Option<u16>,
+    position: (u16, u16),
     layout_mode: LayoutMode,
     sizing: Sizing,
     padding: u16,
     child_gap: u16,
+    color: srgb,
     parent: Weak<Mutex<dyn Container>>,
     children: Vec<Arc<Mutex<dyn Primative>>>,
 }
@@ -113,10 +132,12 @@ impl Default for Rectangle {
             min_height: Default::default(),
             max_width: Default::default(),
             max_height: Default::default(),
+            position: Default::default(),
             layout_mode: Default::default(),
             sizing: Default::default(),
             padding: Default::default(),
             child_gap: Default::default(),
+            color: Default::default(),
             children: Default::default(),
         }
     }
@@ -195,6 +216,14 @@ impl Primative for Rectangle {
         self.perfered_height = height;
     }
 
+    fn get_position(&self) -> (u16, u16) {
+        self.position
+    }
+
+    fn set_position(&mut self, position: (u16, u16)) {
+        self.position = position;
+    }
+
     fn as_container(&mut self) -> std::option::Option<&mut dyn Container> {
         Some(self as &mut dyn Container)
     }
@@ -235,6 +264,28 @@ impl Container for Rectangle {
                 match self.sizing.height {
                     SizingMode::Fixed(h) => self.perfered_height = h,
                     SizingMode::Fit | SizingMode::Grow => self.perfered_height = off_axis_size,
+                }
+            }
+        }
+    }
+
+    fn set_child_positions(&mut self) {
+        match self.layout_mode {
+            LayoutMode::TopToBottom => todo!(),
+            LayoutMode::LeftToRight => {
+                let mut child_position = self.position;
+                child_position.0 += self.padding;
+                child_position.1 += self.padding;
+
+                for child in &self.children {
+                    if let Ok(mut prim) = child.lock() {
+                        prim.set_position(child_position);
+                        child_position.0 += prim.get_width() + self.child_gap;
+
+                        if let Some(container) = prim.as_container() {
+                            container.set_child_positions();
+                        }
+                    }
                 }
             }
         }
